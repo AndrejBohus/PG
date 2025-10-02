34db8b996522cf4ce02af53cadce39cef6c350f5
AndrejBohus <131518484+AndrejBohus@users.noreply.github.com>
2025-10-02 08:49:05 +0000

    Commit for first task

---
+++ cviceni1.py
@@ -0,0 +1,41 @@
+# a="hello world"
+# print(a)
+
+
+# def main():
+#     vstup = input("Zadej číslo:")
+#     vstup = int(vstup)
+#     vstup *= 3
+#     print(f"Trojnásobek čísla: {vstup}")
+#     vstup -= 2
+#     if vstup < 10:
+#         print("Větší než 10")
+#     else:
+#         print("Menší než 10")
+#     # print(f"Trojnásobek čísla: {vstup}")
+# if __name__ == "__main__":
+#     main()
+
+# def main():
+#     vstup = int(input("Zadej vstup:"))
+    
+#     if vstup > 10:
+#         print("X je větší než 10")
+#     elif vstup < 10 :
+#         print("X je menší než 10")
+#     else:
+#         print("Číslo X je rovno 10")
+    
+# if __name__ == "__main__":
+#     main()
+
+# def delitelne_beze_zbytku(cislo):
+#     # return False
+#     if cislo %:
+    
+    
+# if __name__ == "__main__":
+#     if delitelne_beze_zbytku(3):
+#         print("Je delitelne beze zbtytku")
+#     else:
+#         print("Není delitelno bezezbytku")
---
+++ first.py
@@ -0,0 +1,4 @@
+def sudy_nebo_lichy(cislo):
+    
+if __name__ == "__main__":
+    sudy_nebo_lichy()
