5021b469f5de6e4fefa07d6da3cda140c6120986
AndrejBohus <131518484+AndrejBohus@users.noreply.github.com>
2025-10-02 08:56:31 +0000

    Try on task

--- first.py
+++ first.py
@@ -1,4 +1,7 @@
 def sudy_nebo_lichy(cislo):
-    
+    if cislo % 2:
+        print(f"Číslo {cislo} je sudé")
+    else:
+        print(f"Číslo {cislo} je liché")
 if __name__ == "__main__":
-    sudy_nebo_lichy()
+    sudy_nebo_lichy(654168416415)
